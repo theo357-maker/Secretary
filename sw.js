ddabfe41466e80ff321b00c3b2c8a4b9d1ecd4cc
The Pigalle <theombowa2@gmail.com>
2026-02-08 20:05:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,10 +5,15 @@ const SYNC_INTERVAL = 15 * 60 * 1000; // 15 minutes
 
 const urlsToCache = [
   '/',
-  'index.html',
-  'manifest.json',
+  '/index.html',
+  '/manifest.json',
   'icon-72x72.png',
+  'icon-96x96.png',
+  'icon-128x128.png',
+  'icon-144x144.png',
+  'icon-152x152.png',
   'icon-192x192.png',
+  'icon-384x384.png',
   'icon-512x512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js',
